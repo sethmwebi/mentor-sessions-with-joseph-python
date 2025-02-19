bc829bd4d6ab00f23d47964e1bcc3b44382218f5
Seth Mwebi <sethmwebi27@gmail.com>
2025-02-19 18:04:23 +0300

    introduction to python

---
+++ python-introduction/app.py
@@ -0,0 +1,40 @@
+# comment
+# print("Hello World")
+# declare a variable
+# name = "Joseph"
+#
+# # print to the terminal
+# print(name) # name*
+'''
+hey
+now
+brown
+cow
+'''
+# print("Donald")
+number = 12
+# site_name = "pypi.org"
+# print(site_name)
+# site_name = "python.org"
+# print(site_name)
+# pres1, pres2, pres3, pres4 = "Bill Clinton", "Barrack Obama", "Trump", "Joe Biden"
+# print(pres3)
+# pres45 = pres47 = "Donald Trump"
+# print(pres45)
+# print(pres47)
+# // collection literals
+# list
+# students = ["joseph", "ivy", "mark"]
+# # print(students[1])
+# students[1] = "charity"
+# print(students)
+# students = ("joseph", "ivy", "mark")
+# students[0] = "brian"
+# print(students[0])
+# students = {'name': "ivy", 'age': 19, 'course': "Computer Science"}
+# # print(students.get("course"))
+# students.__setitem__("course", "Software Engineering")
+# print(students.get("course"))
+# set
+vowels = {'a', 'e', 'i', 'o', 'u', 'a', 'i', 'e'}
+print(vowels)
