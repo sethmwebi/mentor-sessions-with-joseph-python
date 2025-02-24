9898792871a73c32b9dfb2a57d404bdea7fde74b
Seth Mwebi <sethmwebi27@gmail.com>
2025-02-24 18:05:54 +0300

    control flow

--- control_flow.py
+++ control_flow.py
@@ -6,9 +6,77 @@
 #     print("Greater than 25")
 # else:
 #     print("Negative")
-def pass_fail(score):
-    return "pass" if score >= 50 else "fail"
-    # js
-    # return score >= 50 ? "pass" : "fail"
+# def pass_fail(score):
+#     return "pass" if score >= 50 else "fail"
+#     # js
+#     # return score >= 50 ? "pass" : "fail"
+#
+# print(pass_fail(49))
+# students = ["john", "ivy", "kevin"]
+#
+# for student in students:
+#     print(student)
+# lang = "Python"
+#
+# for char in lang:
+#     print(char)
+# languages = ["Python", "JavaScript", "Typescript", "C++"]
+#
+# print("LANGUAGES THAT I KNOW")
+#
+# for lang in languages:
+#     if lang == "JavaScript":
+#         break
+#     print(lang)
+# attributes = ['Electric', 'Fast']
+# cars = ['Tesla', 'Porsche', 'Mercedes']
+#
+# for car in cars:
+#     for attribute in attributes:
+#         print(car, attribute)
+# number = 1
+#
+# while number <= 10:
+#     print(number)
+#     number += 1
 
-print(pass_fail(49))
+# n = 10
+#
+# if n > 10:
+#     pass
+#
+#
+# print('Hello')
+# from random import choice, shuffle
+# shuffle(students)
+#
+# print(students)
+# import math
+# def calculate_area(radius):
+#     return round(math.pi * math.pow(radius, 2), 2)
+#
+# print(calculate_area(5))
+# student_list_1 = ["kevin", "mark", 'charity', "barrack"]
+# student_list_2 = ["Fonte", "Nickson"]
+# # print(students[:])
+# # students.append("Ivy")
+# # students.insert(0, "Joseph")
+# # print(students)
+# student_list_1.extend(student_list_2)
+# # print(student_list_1)
+# print(len(student_list_1))
+
+numbers = [1, 200, 9, 4, 50]
+
+# def find_largest(numbers):
+#     max_number = 0
+#     for num in numbers:
+#         if num > max_number:
+#             max_number = num
+#     return max_number
+def find_largest(numbers):
+    length = len(numbers) -1
+    return numbers.sort()
+
+print(find_largest(numbers))
+
